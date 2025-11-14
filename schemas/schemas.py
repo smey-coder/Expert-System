eda6cb8ef5242636e952ca6841fdb67b48e6fdd7
Your Name <sanreaksmey01@gmail.com>
2025-11-15 01:58:54 +0700

    Expert System Flask

---
+++ schemas/schemas.py
@@ -0,0 +1,38 @@
+facts_array_schema = {
+    "$schema": "http://json-schema.org/draft/2020-12/schema",
+    "type": "array",
+    "items": {
+        "type":"object",
+        "additionalProperties": False,
+        "required": ["id", "description","value"],
+        "properties": {
+            "id" : {"type": "string", "minLength": 1},
+            "description": {"type": "string"},
+            "value": {"type": "boolean"},
+            "tags" : {"type": "array", "items": {"type": "string"}}
+        }
+    }
+
+}
+
+rules_array_schema = {
+    "$schema": "http://json-schema.org/draft/2020-12/schema",
+    "type": "array",
+    "items": {
+        "type":"object",
+        "additionalProperties": False,
+        "required": ["id", "conditions","conclusion"],
+        "properties": {
+            "id" : {"type": "string", "minLength": 1},
+            "conditions": {
+                "type": "array",
+                "minItems": 1,
+                "items": {"type": "string", "minLength": 1}
+                },
+            "conclusion": {"type": "string", "minLength": 1},
+            "certainty": {"type": "number", "minimum": 0.0, "maximum": 1.0},
+            "explain" : {"type": "string"}
+        }
+    }
+
+}
